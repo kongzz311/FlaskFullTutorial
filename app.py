940bbc250f48504679d203e484d26886346f7264
stevekong <kozenzei@outlook.com>
2021-06-21 23:57:55 +0800

    01-Getting-Started

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return "Hello world!"
+@app.route('/home')
+def home():
+    return "<h1>Home Page</h1>"
 
+
+@app.route('/about')
+def about():
+    return "<h1>About Page</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
